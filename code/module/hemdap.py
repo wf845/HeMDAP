a96f31fd5b1763db6ac68cd7f41171d25acf86e3
wf845 <845824947@qq.com>
2024-01-09 15:38:40 +0800

    算法框架

--- code/module/hemdap.py
+++ code/module/hemdap.py
@@ -2,18 +2,17 @@ import torch.nn as nn
 import torch.nn.functional as F
 from .mp_encoder import Mp_encoder
 from .sc_encoder import *
+
 from .contrast import Contrast
 import torch
 import random
 
-import argparse
 
-class Hemdap(nn.Module):
-    def __init__(self, hidden_dim, feats_dim_list1, feats_dim_list2, feat_drop, attn_drop, P1, P2, sample_rate,sample_rate1,
-                 nei_num, tau, lam,gamma):
-        super(Hemdap, self).__init__()
-        self.gamma = gamma
+class HeCo(nn.Module):
+    def __init__(self, hidden_dim, feats_dim_list1, feats_dim_list2, feat_drop, attn_drop, P1, P2, sample_rate,sample_rate1, nei_num, tau, lam):
+        super(HeCo, self).__init__()
         self.hidden_dim = hidden_dim
+
         self.fc_list2 = nn.ModuleList([nn.Linear(feats_dim, hidden_dim, bias=True)
                                        for feats_dim in feats_dim_list2])
         # print(self.fc_list2)
@@ -32,18 +31,14 @@ class Hemdap(nn.Module):
             self.feat_drop = lambda x: x
 
         self.mp1 = Mp_encoder(P1, hidden_dim, attn_drop)
-
-        config = Configuration()
-        args = config.parse_args()
-        model = MINIMDA(args)
-        self.sc1 = model(graph, miRNA, disease)
+        self.sc1 = Sc_encoder(hidden_dim, sample_rate, nei_num, attn_drop)
 
         self.mp2 = Mp_encoder(P2, hidden_dim, attn_drop)
-        # self.sc2 = Sc_encoder(hidden_dim, sample_rate1, nei_num, attn_drop)
+        self.sc2 = Sc_encoder(hidden_dim, sample_rate1, nei_num, attn_drop)
 
         self.contrast = Contrast(hidden_dim, tau, lam)
 
-    def forward(self, feats1, feats2, mps1, mps2, nei_index1, nei_index2, graph):  # p a s
+    def forward(self, feats1, feats2, mps1, mps2, nei_index1, nei_index2):
 
         h_all1 = []
         h_all2 = []
@@ -53,11 +48,9 @@ class Hemdap(nn.Module):
             h_all2.append(F.elu(self.feat_drop(self.fc_list2[i](feats2[i]))))
 
         z_mp1 = self.mp1(h_all1[0], mps1)
-       # z_sc1 = self.sc1(h_all1, nei_index1)
+        z_sc1 = self.sc1(h_all1, nei_index1)
         z_mp2 = self.mp2(h_all2[0], mps2)
-       # z_sc2 = self.sc2(h_all2, nei_index2)
-
-        z_sc1,  z_sc2 = self.sc1(graph, h_all1[0],h_all1[1])
+        z_sc2 = self.sc2(h_all2, nei_index2)
 
         return z_mp1, z_sc1, z_mp2, z_sc2
 
@@ -84,6 +77,4 @@ class Hemdap(nn.Module):
         z_sc2 = gamma*z_sc2.detach()
         z2 =  torch.add(z_mp2, z_sc2)
 
-        # z1 = self.pca.fit_transform(z1.cpu().numpy())
-        # z2 = self.pca.fit_transform(z2.cpu().numpy())
         return z1, z2
